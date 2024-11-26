abc48c39253b4f95b9d136e7fd6980b2783d7f56
christianwaldmann <68497210+christianwaldmann@users.noreply.github.com>
2024-11-27 00:39:26 +0100

    Fix Github action that runs packer build (#6)
    * fix: add missing reference between jobs
    
    * fix: use existing version
    
    * fix: pass missing variables to packer init
    
    * fix: use correct flag name
    
    ---------
    
    Co-authored-by: christianwaldmann <christianwaldmann>

--- scripts/packer.sh
+++ scripts/packer.sh
@@ -18,7 +18,7 @@ if [ -z "${AWS_SECRET_ACCESS_KEY:-}" ]; then
   exit 1
 fi
 
-packer init .
+packer init -var "aws_access_key_id=${AWS_ACCESS_KEY_ID}" -var "aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}" .
 packer validate .
 
 if [[ "${1:-}" = "validate" ]]; then
