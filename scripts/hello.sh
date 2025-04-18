c4f4fb171819f96c9e73e80e0877db96b5a66397
christianwaldmann <68497210+christianwaldmann@users.noreply.github.com>
2025-04-18 14:44:30 +0200

    Update hello.sh

--- scripts/hello.sh
+++ scripts/hello.sh
@@ -4,7 +4,12 @@ set -euo pipefail
 HERE=$(dirname "$0")
 cd "${HERE}/../terraform"
 
-export TF_VAR_hcloud_token=abc
+if [ -z "${HCLOUD_TOKEN:-}" ]; then
+  echo >&2 "Error: HCLOUD_TOKEN is not set. Please export it before running this script."
+  exit 1
+fi
+
+export TF_VAR_hcloud_token=${HCLOUD_TOKEN}
 export TF_VAR_hcloud_dns_token=def
 export TF_VAR_ssh_public_key=xyz
 
