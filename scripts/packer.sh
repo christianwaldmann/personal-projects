4ce49675c1f71e9efb861feb4c81040c85ad2cfe
christianwaldmann <68497210+christianwaldmann@users.noreply.github.com>
2025-04-20 09:24:19 +0200

    fix: pass variables to packer validate

--- scripts/packer.sh
+++ scripts/packer.sh
@@ -19,7 +19,7 @@ if [ -z "${AWS_SECRET_ACCESS_KEY:-}" ]; then
 fi
 
 packer init -var "aws_access_key_id=${AWS_ACCESS_KEY_ID}" -var "aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}" .
-packer validate .
+packer validate -var "aws_access_key_id=${AWS_ACCESS_KEY_ID}" -var "aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}" .
 
 if [[ "${1:-}" = "validate" ]]; then
     exit 0
