e1653a5b1d0b77f68b3e2b7ecd56314ce9303555
christianwaldmann <christianwaldmann>
2026-05-14 22:04:12 +0200

    fix: use correct resource

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -1,33 +1,37 @@
 
-resource "hcloud_zone_record" "geometrierechner" {
+resource "hcloud_zone_rrset" "geometrierechner" {
     zone    = data.hcloud_zone.this.name
     type    = "A"
     name    = "geometrierechner"
-    value   = hcloud_server.this.ipv4_address
-    comment = "managed by terraform"
+    records = [
+      { value   = hcloud_server.this.ipv4_address, comment = "managed by terraform" }
+    ]
 }
 
-resource "hcloud_zone_record" "bookmarks" {
+resource "hcloud_zone_rrset" "bookmarks" {
     zone    = data.hcloud_zone.this.name
     type    = "A"
     name    = "bookmarks"
-    value   = hcloud_server.this.ipv4_address
-    comment = "managed by terraform"
+    records = [
+      { value   = hcloud_server.this.ipv4_address, comment = "managed by terraform" }
+    ]
 }
 
-resource "hcloud_zone_record" "bookmarks-api" {
+resource "hcloud_zone_rrset" "bookmarks-api" {
     zone    = data.hcloud_zone.this.name
     type    = "A"
     name    = "api.bookmarks"
-    value   = hcloud_server.this.ipv4_address
-    comment = "managed by terraform"
+    records = [
+      { value   = hcloud_server.this.ipv4_address, comment = "managed by terraform" }
+    ]
 }
 
-resource "hcloud_zone_record" "homepage" {
+resource "hcloud_zone_rrset" "homepage" {
     zone    = data.hcloud_zone.this.name
     type    = "A"
     name    = "@"
-    value   = hcloud_server.this.ipv4_address
-    comment = "managed by terraform"
+    records = [
+      { value   = hcloud_server.this.ipv4_address, comment = "managed by terraform" }
+    ]
 }
 
