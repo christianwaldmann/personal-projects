26a6280cfa9ca507ae7ae6aeb7c7f81e494f3cfc
christianwaldmann <christianwaldmann>
2026-05-14 21:19:10 +0200

    chore: migrate dns zone to default project

--- scripts/terraform.sh
+++ scripts/terraform.sh
@@ -9,11 +9,6 @@ if [ -z "${HCLOUD_TOKEN:-}" ]; then
   exit 1
 fi
 
-if [ -z "${HCLOUD_DNS_TOKEN:-}" ]; then
-  echo >&2 "Error: HCLOUD_DNS_TOKEN is not set. Please export it before running this script."
-  exit 1
-fi
-
 if [ -z "${SSH_PUBLIC_KEY:-}" ]; then
   echo >&2 "Error: SSH_PUBLIC_KEY is not set. Please export it before running this script."
   exit 1
@@ -25,7 +20,6 @@ if [ -z "${STORAGEBOX_SSH_PRIVATE_KEY:-}" ]; then
 fi
 
 export TF_VAR_hcloud_token=${HCLOUD_TOKEN}
-export TF_VAR_hcloud_dns_token=${HCLOUD_DNS_TOKEN}
 export TF_VAR_ssh_public_key=${SSH_PUBLIC_KEY}
 export TF_VAR_storagebox_ssh_private_key=${STORAGEBOX_SSH_PRIVATE_KEY}
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,10 +3,6 @@ variable "hcloud_token" {
     sensitive = true
 }
 
-variable "hcloud_dns_token" {
-    sensitive = true
-}
-
 variable "ssh_public_key" {
     sensitive = true
 }
